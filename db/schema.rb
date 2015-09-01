80d45758fd1c1ca8b2d6464540e9d1407ad6f66d
garthomp <garrettdthompson@gmail.com>
2015-08-31 19:03:39 -0500

    add email address model scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817230216) do
+ActiveRecord::Schema.define(version: 20150831234651) do
+
+  create_table "email_addresses", force: :cascade do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string   "EmailAddress"
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
